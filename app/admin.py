799c3bf237ab86ec5d5542d7267129fdcdb98f2f
rinosetiyo <42023075+rinosetiyo@users.noreply.github.com>
2023-08-14 09:46:19 +0700

    modified admin.py

--- app/admin.py
+++ app/admin.py
@@ -11,5 +11,16 @@ class TeamAdmin(admin.ModelAdmin):
     list_display = ('id','thumbnail','first_name','designation','created_at')
     search_fields = ('first_name','last_name','designation')
     list_filter = ('designation',)
+
+class CarAdmin(admin.ModelAdmin):
+    def photo(self, object):
+        return format_html("<img src='{}' width='40' />".format(object.car_photo.url))
+    photo.short_description = 'Car'
+
+    list_display = ('id','photo','car_title','condition','price','milage','fuel_type','created_at','is_featured')
+    search_fields = ('car_title','model')
+    list_editable = ('is_featured',)
+    list_filter = ('condition','price','milage')
+
 admin.site.register(Team, TeamAdmin)
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
